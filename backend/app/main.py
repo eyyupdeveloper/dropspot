5223b687ecc688e206293afa1fd67cf9828f72ab
Eyyüp Tazecan <developers.eyyupcans@gmail.com>
2025-11-06 19:30:27 +0300

    main.py içine waitlist router eklendi

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.routers import auth
 from app.database import Base, engine
 from app.routers import auth, drops
+from app.routers import auth, drops, waitlist
+
 
 
 app = FastAPI(title="DropSpot API")
@@ -16,4 +18,6 @@ def ana_sayfa():
     return {"mesaj": "DropSpot backend çalışıyor"}
     
 app.include_router(drops.router)
-
+app.include_router(auth.router)
+app.include_router(drops.router)
+app.include_router(waitlist.router)
