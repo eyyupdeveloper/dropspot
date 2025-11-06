1b1fa2469d1ee8c627cd87f1a8210f4b68c784f9
Eyyüp Tazecan <developers.eyyupcans@gmail.com>
2025-11-06 19:35:41 +0300

    main.py içine claim router eklendi

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,21 +3,18 @@ from app.routers import auth
 from app.database import Base, engine
 from app.routers import auth, drops
 from app.routers import auth, drops, waitlist
-
-
+from app.routers import auth, drops, waitlist, claim
 
 app = FastAPI(title="DropSpot API")
 
 Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
+app.include_router(drops.router)
+app.include_router(waitlist.router)
+app.include_router(claim.router)
 
 
 @app.get("/")
 def ana_sayfa():
     return {"mesaj": "DropSpot backend çalışıyor"}
-    
-app.include_router(drops.router)
-app.include_router(auth.router)
-app.include_router(drops.router)
-app.include_router(waitlist.router)
